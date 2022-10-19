805baf029c29cfa5763955fb1a6443cead514779
JhonyHC <jonhatan.higuera5180@alumnos.udg.mx>
2022-10-19 16:46:27 -0500

    Se puso jetstream y se va a reprobar este pedo

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,3 +1,60 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Products') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div>
+                    <a href="/products/create">Create Product</a>
+                    <br><br><br>
+                </div>
+            
+                @if (count($products) > 0)
+                <table border="dashed" style="text-align: center;">
+                    <thead>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Category</th>
+                        <th>Description</th>
+                        <th>Stock</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </thead>
+                    <tbody>
+                        @foreach($products as $product)
+                        <tr>
+                            <td><a href="/products/{{ $product->id}}">{{ $product->name}}</a></td>
+                            <td>{{ $product->price}}</td>
+                            <td>{{ $product->category}}</td>
+                            <td>{{ $product->description}}</td>
+                            <td>{{ $product->stock}}</td>
+                            <td><a href="/products/{{ $product->id }}/edit">Update</a></td>
+                            <td>
+                                <form action="/products/{{ $product->id }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="submit" value="delete">
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <ul>
+                </ul>
+                @else
+                    <p>No Products Registered</p>
+                @endif
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+{{-- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -55,4 +112,4 @@
         <p>No Products Registered</p>
     @endif
 </body>
-</html>
+</html> --}}
